2b6fc4789eee61760da95dc4669e24f1b1ab5c69
Tushar Bapu Adling <tusharadling24@gmail.com>
2023-05-07 15:53:52 +0530

    Final commit

---
+++ admin/course.php
@@ -0,0 +1,36 @@
+<!-- <script>
+	function deleteData(id)
+	{
+		if(confirm("You want to delete ?"))
+		{
+		window.location.href="deletecourse.php?course_id="+id;
+		}
+	
+	}
+</script> -->
+
+<?php 
+include('../config.php');
+
+//<!--data display-->
+
+
+echo "<table border='1' class='table table-striped'>";
+
+echo "<tr class='danger'><th colspan='4'><a href='admindashboard.php?info=add_course'>Add New</a></th></tr>";
+
+echo "<Tr><th>Id</th><th>Department</th><th>Update</th><th>Delete</th></tr>";
+
+	$que=mysqli_query($con,"select *  from department");
+	while($res=mysqli_fetch_array($que))
+	{
+	echo "<Tr>";
+	echo "<td>".$res['department_id']."</td>" ;
+	echo "<td>".$res['department_name']."</td>" ;
+	echo "<td><a href='admindashboard.php?info=updatecourse&department_id=$res[department_id]'>Update</a></td>";
+	echo "<td><a href='admindashboard.php?info=deletecourse&department_id=$res[department_id]'>Delete</a></td>";
+	}
+	
+echo "</table>";	
+
+?>
